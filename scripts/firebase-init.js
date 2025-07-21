7750598dbec03cae6c798fdafaae770978400946
annashrop <211416555+annashrop@users.noreply.github.com>
2025-07-21 14:59:55 -0700

    Update firebase-init.js

--- scripts/firebase-init.js
+++ scripts/firebase-init.js
@@ -1,13 +1,14 @@
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCXrZyRjTDmwxYRNNXvbtnc0eLNdbxmPXM",
+  authDomain: "emailoptin-be000.firebaseapp.com",
+  databaseURL: "https://emailoptin-be000-default-rtdb.firebaseio.com",
+  projectId: "emailoptin-be000",
+  storageBucket: "emailoptin-be000.firebasestorage.app",
+  messagingSenderId: "549214862785",
+  appId: "1:549214862785:web:613c2c8435de1533b74340",
+  measurementId: "G-6C92LPC8CF"
 };
 
 firebase.initializeApp(firebaseConfig);
