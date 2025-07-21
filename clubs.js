a6c97541a55e2048251e85724ddc102dd5c2200e
annashrop <211416555+annashrop@users.noreply.github.com>
2025-07-21 14:46:37 -0700

    Add files via upload

---
+++ clubs.js
@@ -0,0 +1,9 @@
+
+const clubs = [
+  { name: "Canyon Gate", region: "West" },
+  { name: "Aliso Viejo", region: "West" },
+  { name: "Oakmont", region: "Northeast" },
+  { name: "Shadowridge", region: "West" },
+  { name: "Firestone", region: "South" },
+  { name: "Hamlet", region: "Northeast" }
+];
